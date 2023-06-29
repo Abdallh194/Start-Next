2aeff1bb753e215adf97fb0da5e59e476dfb120f
Abdallh <Abdallhsabry194@gmail.com>
2023-06-29 17:47:12 +0300

    Pure NextJs Teplate for stating

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ramadan</title>
+        <meta name="description" content="شهر رمضان المعظم" />
+        <link rel="icon" href="/fanoos.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>lorem Ipsum</h1>
+      </main>
+    </>
+  );
+}
